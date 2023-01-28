d047e2f90d6b59c8ae8e87330c874aec51b1eef3
Thiago Borba <thiagoborba8@gmail.com>
2023-01-27 21:52:26 -0300

    Terminando o Layout

--- AgendaDeContatos/Controllers/HomeController.cs
+++ AgendaDeContatos/Controllers/HomeController.cs
@@ -6,13 +6,7 @@ namespace AgendaDeContatos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+            
         public IActionResult Index()
         {
             return View();
